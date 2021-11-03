1b161e55e7a809a34596e78fe9df08ef73f2347f
Tobin Cooney <tobin.cooney@gmail.com>
2021-11-03 19:28:10 -0400

    input stores to variable and writes to json file!

--- jason.py
+++ jason.py
@@ -26,15 +26,19 @@ print(y["player"])
 with open("./toad-archives.json", "r") as archive:
     n = archive.read()
     archive.close()
-    
 k = json.loads(n)
+print("toad-archives.json readout: ")
 print(k)
-print(k["player"])
-#replace value
-k["player"] = "geurauldde"
-print(k)
-print(k["player"])
+print("Player: " + k["player"])
+print()
 
+# replace value
+newt = input("enter new player name: ")
+print(newt)
+# only works in python3 apparently, no clue why. so i just had to specify 'python3 jason.py' but that's probably default for you who knows
+k["player"] = newt
+print(k)
+# write over json file with new data table
 with open("./toad-archives.json", "w") as archive:
     archive.write(json.dumps(k))
     archive.close()
