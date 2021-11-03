9d84a3d88eb6e8885ea106c4fc2a742ff719ce35
Tobin Cooney <tobin.cooney@gmail.com>
2021-11-03 17:59:25 -0400

    cleanup: added a bunch of stuff back in from master, going to merge

--- ToadBot.py
+++ ToadBot.py
@@ -1,19 +1,43 @@
+#you need to run the stuff here for this to all work https://discordpy.readthedocs.io/en/stable/intro.html
+# I also had to run pip install discord to fix an error, but stack overflow has all the info you would need
 import datetime
 import discord
 from discord.ext import commands
 
-import aiofiles
+#Looks like this is still usefull after all, possibly needed for aio json stuff?
 import aiohttp
+import aiofiles
 
 import json
 
+#have a file named "tok" in the same folder with the code DontStealMyToken = "token"
+#DONT FORGET TO ADD THE TOK FILE TO THE GITIGNORE!
 from tok import DontStealMyToken
 
-bot = commands.Bot(command_prefix=")")
+#invite your bot to a server with #https://discord.com/oauth2/authorize?client_id=BOT ID HERE&scope=bot
 
-@bot.command(name="storyp")
-async def storyp(ctx, op, *, msg=None):
-    "plaintext: use ')storyp read' or ')storyp add [words]'."
+#sets the bot (object?) to be referenced with variable "bot". Also sets the command prefix
+bot = commands.Bot(command_prefix="~")
+
+@bot.command(name="test")
+#You need to structure commands like this, but I don't remember why...
+async def hello_world(ctx: commands.Context):
+    #these strings are optional, but show up when ~help is run
+    "Lets you know that the bot is alive"
+    #await is an async command that opens a seperate thread, allowing other commands to be run while this one is ongoing
+    #always try to start the new thread as soon as possible as the compute time for code before it makes the bot freeze for that amount of time
+    await ctx.send("Test passed!")
+
+@bot.command(name="ping")
+async def ping(ctx: commands.Context):
+    "returns the bot's latency in ms"
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+
+#file handler commands
+
+@bot.command(name="io")
+async def io(ctx, op, *, msg=None):
+    "use '~io read' or '~io write <text>'"
     if op == "read":
         async with aiofiles.open("./story-plaintext.txt", "r") as folder:
             readout = await folder.read()
@@ -27,21 +51,35 @@ async def storyp(ctx, op, *, msg=None):
             await folder.write("\n")
             await folder.close()
 
-@bot.command(name="chp")
-async def chp(ctx: commands.Context):
-    "chapter selection menu"
-    await ctx.send("hi sorry this feature doesn't actually exist yet, we'll get there")
+# right now jason function looks the same as file i/o, so we can work on making it deal with .json files, but the basic read/write is working
+# i think basically 'jason' is just our test function right? so like we can make sure 'read' and 'write' work, having one of the function's arguments be the variable and another be the value, and then once that's all figured out it'll be implemented more on the back end?
+"""
+@bot.command(name="jason")
+async def jason(ctx, op, *, msg=None):
+    "use '~jason read' or '~jason write <text>'"
+    if op == "read":
+        async with aiofiles.open("./story-plaintext.txt", "r") as jasper:
+            readout = await jasper.read()
+            print(readout)
+            await ctx.send(readout)
+            await jasper.close()
+    if op == "add":
+        await ctx.send(msg)
+        async with aiofiles.open("./story-plaintext.txt", "a+") as jasper:
+            await jasper.write(msg)
+            await jasper.write("\n")
+            await jasper.close()
+"""
+    
 
-@bot.command(name="_ping")
-async def ping(ctx: commands.Context):
-    "returns the bot's latency in ms"
-    await ctx.send(f"pling! {round(bot.latency * 1000)}ms")
 
 @bot.command(name="stop", aliases=['shutdown', 'end', 'quit', 'exit'])
-@commands.is_owner()
+@commands.is_owner() #I guess you can add tags like this to specify checks before running a command
+#I stole from the internet though, so don't really know how they work
 async def shutdown(ctx):
     "shuts down bot (provided command issuer is the same as dev acc for bot)"
     await ctx.send("toad bot is departing")
+    # output the story so far to backup.txt with timestamp when you exit
     async with aiofiles.open("./story-plaintext.txt", "r") as latest:
         readout = await latest.read()
         async with aiofiles.open("./backup.txt", "a+") as bkp:
@@ -52,9 +90,17 @@ async def shutdown(ctx):
         await latest.close()
     await ctx.bot.close()
 
+
+#this is the general catch for errors, not very clean, but it works
 @bot.event
 async def on_command_error(ctx, error):
+    #my approach to this is just have an if else chain to catch errors...
     if isinstance(error, discord.ext.commands.errors.NotOwner):
-        await ctx.send("foolish mortal, you have no power over TobiToadBoat")
+        await ctx.send("You are not cool enough to do that.")
 
+#this starts the loop that is the bot
+#the code essentialy gets stuck here as it just constantly loops over the @bot.whatever things waiting for triggers
+#I think the loop starts at bot = commands.Bot() but am not sure
 bot.run(DontStealMyToken)
+
+#if you want code to run after the bot is shut down, put it here
