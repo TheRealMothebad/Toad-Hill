e48845f332cb7c0dd20810dec7226dcca1c62fa8
Tobin Cooney <tobin.cooney@gmail.com>
2021-11-03 18:35:29 -0400

    read json object from file

--- jason.py
+++ jason.py
@@ -1,4 +1,14 @@
+"""
+import discord
+from discord.ext import commands
+
+import aiohttp
+import aiofiles
+"""
+
 import json
+
+# very basic parsing
 
 x = '{ "player":"gerald", "sector":"main hall", "toads":74 }'
 
@@ -6,3 +16,16 @@ y = json.loads(x)
 
 print(y)
 print(y["player"])
+
+
+
+
+
+# read a file
+
+with open("./toad-archives.json", "r+") as archive:
+    n = archive.read()
+    k = json.loads(n)
+    print(k)
+    print(k["player"])
+    archive.close()
