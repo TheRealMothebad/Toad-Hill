13b06ea92e98e392526740715dd82568e059385f
Tobin Cooney <tobin.cooney@gmail.com>
2022-11-07 21:00:00 -0500

    if/else menu afalkfjalf

--- bash-game/main.sh
+++ bash-game/main.sh
@@ -3,8 +3,34 @@
 map=""
 
 function menu {
-	map=$( cat maps/menu.txt )
-	clear && echo -n "$map"
+	clear && echo "
+
+	Hello and welcome to Toad. it is.
+	please enjoy it.
+
+
+		[N] Start from the beginning
+		[C] Continue a saved game
+		[Q] Quit
+
+"
+	echo -n "	: "
+	read meninpt
+	if [ "$mininpt" == "N" ] || [ "$meninpt" == "n" ]
+	then
+		echo "selected start"
+	elif [ "$mininpt" == "C" ] || [ "$meninpt" == "c" ]
+	then
+		echo "elected continue"
+	elif [ "$mininpt" == "Q" ] || [ "$meninpt" == "q" ]
+	then
+		exit
+	else
+		echo "That was not a valid selection. Please try again."
+		sleep 1
+		menu
+	fi
 }
 
 menu
+echo "end"
