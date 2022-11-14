1bb7873f1dc16432c2e7de2dc32af7f2eb72d74a
Tobin Cooney <tobin@tobincooney.com>
2022-11-13 23:38:13 -0500

    I DID WASD.

--- bash-game/keypress.sh
+++ bash-game/keypress.sh
@@ -1,12 +1,56 @@
 #!/bin/bash
 
+posx=$(( $( tput cols ) / 2 ))
+posy=$(( $( tput lines) / 2 ))
+
+
+echo -n "cols: "
+tput cols
+echo -n "lines: "
+tput lines
+echo "q	quit
+w	up
+a	left
+s	down
+d	right
+"
+
+
+function plant {
+	clear && echo "planting [$posx, $posy]"
+	n=0
+	while [ "$n" -lt $posy ]
+	do
+		n=$(( $n + 1 ))
+		echo ""
+	done
+	n=0
+	while [ "$n" -lt $posx ]
+	do
+		n=$(( $n + 1 ))
+		echo -n " "
+	done
+}
+
 
 function nav {
 	while [ true ]
 	do
+		plant
 		read -s -n 1 key
-		echo ":: $key ::"
-		if [ $key == "q" ]
+		if [ $key == "w" ]
+		then
+			posy=$(( $posy - 1 ))
+		elif [ $key == "a" ]
+		then
+			posx=$(( $posx - 1 ))
+		elif [ $key == "s" ]
+		then
+			posy=$(( $posy + 1 ))
+		elif [ $key == "d" ]
+		then
+			posx=$(( $posx + 1 ))
+		elif [ $key == "q" ]
 		then
 			exit
 		fi
