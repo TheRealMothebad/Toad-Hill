f3f60e53e69c4ee3d62bfe083cacf7d0bb8d4b8a
Tobin Cooney <tobin@tobincooney.com>
2022-11-14 03:03:50 -0500

    bot: (broken) object placement noodling

--- bash-game/keypress.sh
+++ bash-game/keypress.sh
@@ -14,34 +14,128 @@ mapy="0"
 
 
 
+objects=("002 012 a" "003 013 b")
+
+
+
+
 
 function plant {
 	clear && echo "planting [$posx, $posy] on map [$mapx, $mapy]"
-	n=0 ; while [ "$n" -lt $posy ]
+
+	# add zeros before posx & posy, up to 3 digits
+	xzpad=""
+	n=0 ; while [ "$n" -lt $(( 3 - ${#posx} )) ]
 	do
+		xzpad+="0"
 		n=$(( $n + 1 ))
-		echo ""
 	done
-	xgap=""
-	n=0 ; while [ "$n" -lt $posx ]
+	plrx="$xzpad$posx"
+
+	yzpad=""
+	n=0 ; while [ "$n" -lt $(( 3 - ${#posy} )) ]
 	do
+		yzpad+="0"
 		n=$(( $n + 1 ))
-		xgap+=" "
 	done
-	echo -n "$xgap"
-	echo -n "#" && echo -ne "\r$xgap"
+	plry="$yzpad$posy"
+
+	# add character to objects list
+
+	tmpobj=("${objects[*]}")
+	tmpobj+=("$plry $plrx #")
+
+	# (i don't understand this next line but it sorts it)
+	IFS=$'\n' tmpobj=($(sort <<<"${tmpobj[*]}"))
+	unset IFS
+
+	echo "${tmpobj[*]}"
+
+
+	yacc="0"
+	xacc="0"
+	n=0 ; while [ "$n" -lt ${#tmpobj[@]} ]
+	do
+		m=0
+		for i in ${tmpobj[$n]}
+		do
+			m=$(( $m + 1 ))
+			if [ $m == "1" ] ; then
+				objy=$i
+			elif [ $m == "2" ] ; then
+				objx=$i
+			elif [ $m == "3" ] ; then
+				obj=$i
+			fi
+		done
+
+		# remove leading zeros
+		objy=$( echo "$objy" | sed 's/^0*//' )
+		objx=$( echo "$objx" | sed 's/^0*//' )
+
+		if [ $objx -ge $domain ] || [ $objx -lt 0 ] || [ $objy -ge $(( $range - 1 )) ] || [ $objy -lt 0 ]
+		then
+			return
+		else
+			if [ $objy == $yacc ] && [ $objx == $xacc ]
+			then
+				echo -ne "\r"
+				xgap=""
+				m=0 ; while [ "$m" -lt $objx ]
+				do
+					m=$(( $m + 1 ))
+					xgap+=" "
+				done
+				echo -ne "$xgap!\r$xgap"
+			elif [ $objy == $yacc ] && ! [ $objx == $xacc ]
+			then
+				xtogo=$(( $objx - $xacc ))
+				xacc="$objx"
+				xgap=""
+				m=0 ; while [ "$m" -lt $xtogo ]
+				do
+					m=$(( $m + 1 ))
+					xgap+=" "
+				done
+				echo -ne "$xgap$obj\r$xgap"
+			else
+				ytogo=$(( $objy - $yacc ))
+
+				m=0 ; while [ "$m" -lt $ytogo ]
+				do
+					m=$(( $m + 1 ))
+					echo ""
+				done
+
+				xgap=""
+				m=0 ; while [ "$m" -lt $objx ]
+				do
+					m=$(( $m + 1 ))
+					xgap+=" "
+				done
+				echo -ne "$xgap$obj\r$xgap"
+
+				xacc="$objx"
+				yacc=$(( $yacc + $objy ))
+			fi
+		fi
+		n=$(( $n + 1 ))
+	done
+
 	#remy=$(( $range - $posy - 1 ))
 	# this is because of the "planting [x, y]" printout, it won't be there in the game unless there are stats or something
-	#remy=$(( $range - $posy - 2 ))
-	#n=0 ; while [ "$n" -lt $remy ]
+
+	#yrem=$(( $range - $yacc - 2 ))
+	#n=0 ; while [ "$n" -lt $yrem ]
 	#do
-		#n=$(( $n + 1 ))
-		#echo ""
+	#	n=$(( $n + 1 ))
+	#	echo ""
 	#done
+
 	#n=0 ; while [ "$n" -lt $domain ]
 	#do
-		#n=$(( $n + 1 ))
-		#echo -n " "
+	#	n=$(( $n + 1 ))
+	#	echo -n " "
 	#done
 }
 
