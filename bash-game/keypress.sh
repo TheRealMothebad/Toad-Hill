6cb3f159ca074ec6afad1098d9792bc0d5904f1e
Tobin Cooney <tobin@tobincooney.com>
2022-11-14 01:01:37 -0500

    bot: added infinite maps

--- bash-game/keypress.sh
+++ bash-game/keypress.sh
@@ -3,75 +3,102 @@
 domain=$( tput cols )
 range=$( tput lines )
 
-posx=$(( $domain / 2 ))
-posy=$(( $range / 2 ))
+centx=$(( $domain / 2 ))
+centy=$(( $range / 2 ))
 
+posx="$centx"
+posy="$centy"
 
-echo -n "cols: "
-tput cols
-echo -n "lines: "
-tput lines
-echo "q	quit
-w	up
-a	left
-s	down
-d	right
-"
+mapx="0"
+mapy="0"
 
 
 
 
 function plant {
-	clear && echo "planting [$posx, $posy]"
+	clear && echo "planting [$posx, $posy] on map [$mapx, $mapy]"
 	n=0 ; while [ "$n" -lt $posy ]
 	do
 		n=$(( $n + 1 ))
 		echo ""
 	done
+	xgap=""
 	n=0 ; while [ "$n" -lt $posx ]
 	do
 		n=$(( $n + 1 ))
-		echo -n " "
+		xgap+=" "
 	done
-	echo -n "#"
+	echo -n "$xgap"
+	echo -n "#" && echo -ne "\r$xgap"
 	#remy=$(( $range - $posy - 1 ))
 	# this is because of the "planting [x, y]" printout, it won't be there in the game unless there are stats or something
-	remy=$(( $range - $posy - 2 ))
-	n=0 ; while [ "$n" -lt $remy ]
-	do
-		n=$(( $n + 1 ))
-		echo ""
-	done
-	n=0 ; while [ "$n" -lt $domain ]
-	do
-		n=$(( $n + 1 ))
-		echo -n " "
-	done
+	#remy=$(( $range - $posy - 2 ))
+	#n=0 ; while [ "$n" -lt $remy ]
+	#do
+		#n=$(( $n + 1 ))
+		#echo ""
+	#done
+	#n=0 ; while [ "$n" -lt $domain ]
+	#do
+		#n=$(( $n + 1 ))
+		#echo -n " "
+	#done
 }
 
 
 function nav {
-	while [ true ]
-	do
-		plant
-		read -s -n 1 key
-		if [ $key == "w" ] || [ $key == "k" ]
-		then
-			posy=$(( $posy - 1 ))
-		elif [ $key == "a" ] || [ $key == "h" ]
-		then
-			posx=$(( $posx - 1 ))
-		elif [ $key == "s" ] || [ $key == "j" ]
-		then
-			posy=$(( $posy + 1 ))
-		elif [ $key == "d" ] || [ $key == "l" ]
-		then
-			posx=$(( $posx + 1 ))
-		elif [ $key == "q" ]
-		then
-			exit
-		fi
-	done
+	read -s -n 1 key
+	if [ $key == "w" ] || [ $key == "k" ]
+	then
+		posy=$(( $posy - 1 ))
+	elif [ $key == "a" ] || [ $key == "h" ]
+	then
+		posx=$(( $posx - 1 ))
+	elif [ $key == "s" ] || [ $key == "j" ]
+	then
+		posy=$(( $posy + 1 ))
+	elif [ $key == "d" ] || [ $key == "l" ]
+	then
+		posx=$(( $posx + 1 ))
+	elif [ $key == "q" ]
+	then
+		exit
+	fi
 }
 
-nav
+function navloop {
+	#while [ $posx -lt $domain ] && [ $posy -lt $range ]
+	# this is because of the "planting [x, y]" printout, it won't be there in the game unless there are stats or something
+	while [ $posx -lt $domain ] && [ $posx -ge 0 ] && [ $posy -lt $(( $range - 1 )) ] && [ $posy -ge 0 ]
+	do
+		plant
+		nav
+	done
+	if [ $posx -ge $domain ] ; then
+		echo "posx exceeds domain"
+		mapx=$(( $mapx + 1 ))
+		posx="0"
+		navloop
+	elif [ $posx -lt 0 ] ; then
+		echo "posx less than 0"
+		mapx=$(( $mapx - 1 ))
+		posx=$(( $domain - 1 ))
+		navloop
+	elif [ $posy -ge $(( $range - 1 )) ] ; then
+		echo "posy exceeds range"
+		mapy=$(( $mapy + 1 ))
+		posy="0"
+		navloop
+	elif [ $posy -lt 0 ] ; then
+		echo "posy less than 0"
+		mapy=$(( $mapy - 1 ))
+		posy=$(( $range - 2 ))
+		navloop
+	fi
+}
+
+function main {
+	navloop
+}
+
+main
